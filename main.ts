ac9134715b6820af9c42d8284bd0da40be13f6fd
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-13 18:57:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,54 @@
+input.onPinPressed(TouchPin.P0, function () {
+    while (Pause == 2) {
+        if (Enemy_Variable == true) {
+            if (Player.isTouching(Enemy)) {
+                game.addScore(1)
+                Enemy.delete()
+                Pause = 1
+                Enemy_Variable = true
+            } else if (Enemy.get(LedSpriteProperty.Y) == 4) {
+                Enemy.delete()
+                Pause = 1
+                Enemy_Variable = true
+            }
+        }
+    }
+    Pause = 0
+})
 input.onButtonPressed(Button.A, function () {
     Player.move(-1)
-})
-input.onPinPressed(TouchPin.P2, function () {
-    game.pause()
-    basic.showNumber(Points)
-    basic.pause(2000)
-    game.resume()
 })
 input.onButtonPressed(Button.B, function () {
     Player.move(1)
 })
 input.onPinPressed(TouchPin.P1, function () {
-    if (Pause == 0) {
-        game.pause()
-        Pause = 1
-    } else if (Pause == 1) {
-        game.resume()
-        Pause = 0
-    }
+    basic.showNumber(game.score())
 })
 input.onGesture(Gesture.Shake, function () {
-    Enemy_Variable = 1
-    if (Enemy_Variable == 1) {
-        Enemy = game.createSprite(randint(1, 4), 0)
-        while (true) {
-            basic.pause(300)
-            Enemy.change(LedSpriteProperty.Y, 1)
-            basic.pause(300)
-            if (Player.isTouching(Enemy)) {
-                Points += 1
-                Enemy.set(LedSpriteProperty.X, randint(1, 4))
-                Enemy.set(LedSpriteProperty.Y, 0)
-            } else if (Enemy.get(LedSpriteProperty.Y) == 4) {
-                Enemy.set(LedSpriteProperty.X, randint(1, 4))
-                Enemy.set(LedSpriteProperty.Y, 0)
-            }
+    Pause = 1
+    Enemy_Variable = true
+    Enemy = game.createSprite(randint(0, 4), 0)
+    while (Enemy_Variable == true) {
+        basic.pause(150)
+        Enemy.change(LedSpriteProperty.Y, 1)
+        basic.pause(150)
+        if (Enemy.get(LedSpriteProperty.Y) == 4 && Pause == 2) {
+            Enemy.delete()
+            game.pause()
+            break;
+        } else if (Player.isTouching(Enemy)) {
+            game.addScore(1)
+            Enemy.set(LedSpriteProperty.X, randint(0, 4))
+            Enemy.set(LedSpriteProperty.Y, 0)
+        } else if (Enemy.get(LedSpriteProperty.Y) == 4) {
+            Enemy.set(LedSpriteProperty.X, randint(0, 4))
+            Enemy.set(LedSpriteProperty.Y, 0)
         }
     }
 })
 let Enemy: game.LedSprite = null
+let Enemy_Variable = false
 let Pause = 0
-let Points = 0
-let Enemy_Variable = 0
 let Player: game.LedSprite = null
 Player = game.createSprite(2, 4)
-Enemy_Variable = 0
-Points = 0
-Pause = 0
+game.setScore(0)
