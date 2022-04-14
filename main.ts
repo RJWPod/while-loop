60e72b098807915f46d70253796927ef7e32b6ce
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-13 20:48:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,8 @@
 input.onPinPressed(TouchPin.P0, function () {
-    while (Pause == 2) {
-        if (Enemy_Variable == true) {
-            if (Player.isTouching(Enemy)) {
-                game.addScore(1)
-                Enemy.delete()
-                Pause = 1
-                Enemy_Variable = true
-            } else if (Enemy.get(LedSpriteProperty.Y) == 4) {
-                Enemy.delete()
-                Pause = 1
-                Enemy_Variable = true
-            }
-        }
+    if (Enemy.isDeleted()) {
+        game.pause()
     }
-    Pause = 2
+    Pause = 1
 })
 input.onButtonPressed(Button.A, function () {
     Player.move(-1)
@@ -22,21 +11,22 @@ input.onButtonPressed(Button.B, function () {
     Player.move(1)
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showNumber(game.score())
+    basic.showNumber(Points)
 })
 input.onGesture(Gesture.Shake, function () {
-    Pause = 1
+    game.resume()
+    Pause = 0
     Enemy = game.createSprite(randint(0, 4), 0)
     while (true) {
         basic.pause(150)
         Enemy.change(LedSpriteProperty.Y, 1)
         basic.pause(150)
-        if (Enemy.get(LedSpriteProperty.Y) == 4 && Pause == 2) {
+        if (Enemy.get(LedSpriteProperty.Y) == 4 && Pause == 1) {
             Enemy.delete()
             break;
         }
         if (Player.isTouching(Enemy)) {
-            game.addScore(1)
+            Points += 1
             Enemy.set(LedSpriteProperty.X, randint(0, 4))
             Enemy.set(LedSpriteProperty.Y, 0)
         } else if (Enemy.get(LedSpriteProperty.Y) == 4) {
@@ -45,11 +35,13 @@ input.onGesture(Gesture.Shake, function () {
         }
     }
 })
-let Enemy: game.LedSprite = null
-let Enemy_Variable = false
+let Points = 0
 let Pause = 0
+let Enemy: game.LedSprite = null
 let Player: game.LedSprite = null
 Player = game.createSprite(2, 4)
+let Enemy_Variable = true
+Enemy = game.createSprite(randint(0, 4), 0)
+Enemy.delete()
 Pause = 0
-Enemy_Variable = true
-game.setScore(0)
+Points = 0
